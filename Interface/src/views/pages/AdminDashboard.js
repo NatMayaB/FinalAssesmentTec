68d419c8b7fa046eb50d1a7360a0cd135a4bcdb4
Natalia Maya <nmaya0309@gmail.com>
2025-06-03 18:56:03 -0600

    sketch done need to do some modification on design

--- Interface/src/views/pages/AdminDashboard.js
+++ Interface/src/views/pages/AdminDashboard.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppHeader from '../../components/AppHeader';
 import '../../scss/AdminDashboard.scss';
 
 const AdminDashboard = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+  const [modalTitle, setModalTitle] = useState('');
+
+  const handleShowModal = (title, content) => {
+    setModalTitle(title);
+    setModalContent(content);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setModalContent('');
+    setModalTitle('');
+  };
+
   return (
     <>
       <AppHeader />
@@ -23,8 +39,8 @@ const AdminDashboard = () => {
               <tr>
                 <td>@usuario1</td>
                 <td>10:00 AM</td>
-                <td>print('Hola')</td>
-                <td>Hola</td>
+                <td className="clickable-cell" onClick={() => handleShowModal('Input', "print('Hola')\nprint('Otra línea de código')")}>print('Hola')</td>
+                <td className="clickable-cell" onClick={() => handleShowModal('Output', 'Hola\nResultado de la ejecución...')}>Hola</td>
                 <td>
                   <button className="delete-user-btn">Borrar usuario</button>
                 </td>
@@ -33,6 +49,17 @@ const AdminDashboard = () => {
             </tbody>
           </table>
         </div>
+
+        {/* Modal para mostrar texto completo */}
+        {modalOpen && (
+          <div className="modal-overlay" onClick={handleCloseModal}>
+            <div className="admin-modal" onClick={e => e.stopPropagation()}>
+              <h3>{modalTitle}</h3>
+              <pre className="modal-content">{modalContent}</pre>
+              <button className="close-modal-btn" onClick={handleCloseModal}>Cerrar</button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
